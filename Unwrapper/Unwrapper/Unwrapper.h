e1f9503593377078d71428702dbe9b164dac7ef4
SaeedRahmatolahi <saeedrahmatolahi.sr@gmail.com>
2021-05-26 10:47:06 +0700

    init framework

---
+++ Unwrapper/Unwrapper/Unwrapper.h
@@ -0,0 +1,18 @@
+//
+//  Unwrapper.h
+//  Unwrapper
+//
+//  Created by Saeed Rahmatolahi on 26/5/2564 BE.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Unwrapper.
+FOUNDATION_EXPORT double UnwrapperVersionNumber;
+
+//! Project version string for Unwrapper.
+FOUNDATION_EXPORT const unsigned char UnwrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Unwrapper/PublicHeader.h>
+
+
